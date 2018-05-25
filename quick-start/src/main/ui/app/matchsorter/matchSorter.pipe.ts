63ada2ff4247770b527406fd044ddd546720b800
Scott D Brooks <clockworked24.7@gmail.com>
2018-05-25 07:14:05 -0700

    MatchSorter Pipe refinement, removed package dependency
     * Now filters and sorts by multiple fields

--- quick-start/src/main/ui/app/matchsorter/matchSorter.pipe.ts
+++ quick-start/src/main/ui/app/matchsorter/matchSorter.pipe.ts
@@ -1,15 +1,29 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import matchSorter from 'match-sorter';
+import * as _ from 'lodash';
 
 @Pipe({
     name: 'matchSorter',
 })
-export class MatchSorterPipe implements PipeTransform {
-    transform(input: any[] = [], options: Object|string, value: string): any {
-        return matchSorter(
-            input,
-            value,
-            typeof options === 'string' ? { keys: [options] } : options,
-        );
+export class MatchSorterPipe implements PipeTransform { 
+    transform(input: any[] = [], options: Array|string, value: string): any {
+      let filteredInput = input;
+      value = String(value).toLowerCase();  // normalzied to lowercase
+
+      if (value.length)
+        filteredInput = _.filter(input, function(i) { 
+          let found = false;
+          _.forEach(options, (field) => {
+            let fieldVal = String(i[field]).toLowerCase();
+            if (fieldVal.indexOf(value) !== -1 ) {
+              found = true;
+              return false; // forEach break loop
+            }
+          })
+          return found; 
+        });
+
+      filteredInput = _.sortBy(filteredInput, options);
+      
+      return filteredInput;
     }
 }
